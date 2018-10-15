26296fd7cf0fec81165af7918e183896067b961e
josh <hello@joshcummingsdesign.com>
2018-10-14 19:44:29 -0700

    adds image sizes

--- www/html/wp-content/plugins/vanicacummings/Helpers/Theme/ImageSizes.php
+++ www/html/wp-content/plugins/vanicacummings/Helpers/Theme/ImageSizes.php
@@ -25,6 +25,11 @@ class ImageSizes {
    * https://developer.wordpress.org/reference/functions/add_image_size/
    */
   public function add() {
-    add_image_size('medium', 800, 600);
+    add_image_size('small_square', 300, 300);
+    add_image_size('medium_square', 480, 480);
+    add_image_size('large_square', 600, 600);
+    add_image_size('post_thumb', 600, 350);
+    add_image_size('large', 800, 600);
+    add_image_size('full_width', 1440, 600);
   }
 }
